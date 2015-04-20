64e108b8557922841c1737c7c7eed461299b1658
kitaKist <kitaKist@users.noreply.github.com>
2015-04-20 15:24:13 +0900

    プロジェクトの最初のコミット

---
+++ Janken/Janken/InitialScreen_Text.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Janken
+{
+    public partial class InitialScreen_Text : UserControl
+    {
+        public InitialScreen_Text()
+        {
+            InitializeComponent();
+        }
+    }
+}
