a3c7dc345fa77d62e1c669fcee69c891aa426fad
kitaKist <kitaKist@users.noreply.github.com>
2015-04-21 15:31:53 +0900

    ゲームデータクラスの追加

---
+++ Janken/GameData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Janken
+{
+    public class GameData
+    {
+        #region
+
+        /**************************************
+         * ゲーム定数の定義
+         **************************************/
+
+        static enum GameProgressStatus
+        {
+            StartScreen,        /* スタート画面 */
+            GamePlay            /* ゲームプレイ画面 */
+        }
+
+        static enum GamePlayStatus
+        {
+            HandSelect,         /* 手の選択画面 */
+            SelectContinue,     /* 続けるかの選択画面 */
+            None                /* それ以外 */
+        }
+
+        #endregion
+
+
+
+        #region
+
+        /**************************************
+         * 場面の処理メソッド
+         **************************************/
+
+        /// <summary>
+        /// ゲームのスタート画面の処理メソッド
+        /// </summary>
+        /// <returns>正常終了時: 0。それ以外: -1</returns>
+        static int Start_Screen()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// ゲームプレイ画面の処理メソッド
+        /// </summary>
+        /// <returns>正常終了時: 0。それ以外: -1</returns>
+        static int GamePlay()
+        {
+            return 0;
+        }
+
+        #endregion
+    }
+}
